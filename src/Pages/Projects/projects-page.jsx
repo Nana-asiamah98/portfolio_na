ff9c8a35c205c3adecb599315e041cd7f39abcd5
judeas <judeas@stlghana.com>
2022-10-13 19:14:54 +0000

    Card updates

--- src/Pages/Projects/projects-page.jsx
+++ src/Pages/Projects/projects-page.jsx
@@ -6,7 +6,7 @@ import Button from "../../Components/Button/Button";
 import DarkMenu from "../../Components/Modal/DarkMenu";
 import "../Projects/project-page.style.css";
 
-const ProjectsPage = ({}) => {
+const ProjectsPage = () => {
   const [isMenuOpened, setIsMenuOpened] = useState(false);
 
   return (
@@ -39,11 +39,35 @@ const ProjectsPage = ({}) => {
         </div>
 
         <section className="main-section">
-            <div className="mt-5">
-                <div className="card-section">
-                    <span>Hello World</span>
+          <div className="mt-5">
+            <div className="card">
+              <div className="card-header">
+                <img
+                  src={`https://unsplash.com/photos/SyYmXSDnJ54/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjY1Njg1NDA5&force=true&w=640`}
+                  alt="unsplash-img"
+                  width={100}
+                />
+              </div>
+              <div className="card-body">
+                <div className="card-body-header">
+                  <h2>Project 1</h2>
                 </div>
+                <div className="card-body-main">
+                  <span>
+                    It is a long established fact that a reader will be
+                    distracted by the readable content of a page when looking at
+                    its layout. The point of using Lorem Ipsum is that it has a
+                    more-or-less normal distribution of letters, as opposed to
+                    using 'Content here, content here', making it look like
+                    readable English.
+                  </span>
+                </div>
+                <div className="card-body-footer">
+                  <span className="card-hyperlink-modal">Project 1</span>
+                </div>
+              </div>
             </div>
+          </div>
         </section>
       </div>
       <AnimatePresence
